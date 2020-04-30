9230ce52f7ba35d362bc15496a7387657b569b87
kaszaildo <63969428+kaszaildo@users.noreply.github.com>
2020-04-30 10:31:06 -0500

    jquery first examples

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
 console.log('Scritp file is loaded.');
+
+// Third part:
+// $('css selector').action()
+// use the same structure
+$('h1').html('This is done by <em>Javascript!</em>');
+// why dont' we use in html: asnwer we don't change html becasuse from JS we can change something when it happens
+
+// $ ('tag').styel('border','1px solid red');
+$ ('p').css('border','1px solid red');
+$ ('p').css('padding','10px');
+
+// how to make classes and force css to do it
+// adding class
+$ ('h2').toggleClass('blue');
+
+// remove it
+$ ('h2').toggleClass('blue');
+
+
+// $('h1').text('This is done by <em>Javascript!</em>');
+// $('h1').html('This is done by <em>Javascript!</em>');
+
+// single line comment//
+
+/* 
+Multiple 
+lines
+of
+comments*/
+
+// First part: copy from Adam//
+
+
+

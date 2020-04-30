6ec74a57f621ff598237dbfe74598fe40431aadc
kaszaildo <63969428+kaszaildo@users.noreply.github.com>
2020-04-30 10:56:16 -0500

    add third part

--- scripts.js
+++ scripts.js
@@ -7,16 +7,41 @@ $('h1').html('This is done by <em>Javascript!</em>');
 // why dont' we use in html: asnwer we don't change html becasuse from JS we can change something when it happens
 
 // $ ('tag').styel('border','1px solid red');
-$ ('p').css('border','1px solid red');
-$ ('p').css('padding','10px');
+
 
 // how to make classes and force css to do it
 // adding class
 $ ('h2').toggleClass('blue');
-
+// $('main').append('<p>A p is appended in the main.</p>');
+// $ ('p').css('border','1px solid red'); 
 // remove it
 $ ('h2').toggleClass('blue');
 
+// append
+// $('main').append('<p>A p is appended in the main.</p>');
+
+// like creating names: let name = 'Adam";
+
+// create a function
+function myLittleFunction (){
+    $('main').append('<p>A p is appended in the main.</p>');
+    $ ('p').css('border','1px solid red');
+    $ ('p').css('padding','10px');
+}
+
+// howe to use function
+myLittleFunction();
+myLittleFunction();
+
+$ ('#add').click(myLittleFunction);
+
+$('#remove').click(function (){
+    $('p:last-child').remove();
+});
+
+
+remove
+// $ ('#add').click()
 
 // $('h1').text('This is done by <em>Javascript!</em>');
 // $('h1').html('This is done by <em>Javascript!</em>');
